1c09f8fd528279b3822236d4ea8ef78b761ab9f3
Khang <khangvq.23itb@vku.udn.vn>
2024-06-22 14:05:15 +0700

    Separated into user and client

--- src/main/java/org/example/onlinechessgame/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example.onlinechessgame;
-
-public class Main {
-    public static void main(String[] args) {
-        ChessApp.main(args);
-    }
-}
--- src/main/java/org/example/onlinechessgame/pieces/PieceType.java
+++
@@ -1,5 +0,0 @@
-package org.example.onlinechessgame.pieces;
-
-public enum PieceType {
-    KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN
-}
